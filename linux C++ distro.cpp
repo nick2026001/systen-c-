e23d31d9aec147d18540f1546d413fda71cc33e1
nick2026001 <nicolasrpereira130@gmail.com>
2026-04-10 12:20:12 -0300

    Add files via upload

---
+++ linux C++ distro.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+#include <cstdint>
+int main()
+{
+    int merory = 1073741824;
+    uint32_t memory [4096];
+    std::string ent;
+    int loop = true;
+    std::cout << "__________________________________________________________________________________________" << std::endl;
+    std::cout << "| [++] C++ LINUX OS v1.0   |   Apps   Places   System   |    "<<merory<<" KB's   [#] [!] |" << std::endl;
+    std::cout << "|========================================================================================|" << std::endl;
+
+    // Desktop Area
+    std::cout << "|                                                                                        |" << std::endl;
+    std::cout << "|   [  Home  ]                                                           .------------.  |" << std::endl;
+    std::cout << "|     /--\\                                                               | TERMINAL   |  |" << std::endl;
+    std::cout << "|    |    |                                                              | >_         |  |" << std::endl;
+    std::cout << "|     \\__/                                                               |            |  |" << std::endl;
+    std::cout << "|                                                                        |            |  |" << std::endl;
+    std::cout << "|   [ Trash  ]             __________________________                    |            |  |" << std::endl;
+    std::cout << "|     |XX|                /                          \\                   |            |  |" << std::endl;
+    std::cout << "|     |__|               |      WELCOME TO C++ OS     |                  '------------'  |" << std::endl;
+    std::cout << "|                         \\__________________________/                                   |" << std::endl;
+    std::cout << "|                                                                                        |" << std::endl;
+    std::cout << "|   [Compiler]                                                                           |" << std::endl;
+    std::cout << "|     {OO}                                              [ (X) Close ] [ (-) Min ]        |" << std::endl;
+    std::cout << "|                                                                                        |" << std::endl;
+
+    // Bottom Bar - Taskbar / Dock
+    std::cout << "|========================================================================================|" << std::endl;
+    std::cout << "|  [Menu]  ||  ( ) Browser  |  (>) Code  |  (#) Terminal  |  (+) Files  ||  [V] [^] [@@] |" << std::endl;
+    std::cout << "|________________________________________________________________________________________|" << std::endl;
+    while (loop = true){
+        std::cin >> ent;
+
+        if (ent == "brow"){
+            std::cout << "__________________________________________________________________________________________________________" << std::endl;
+    std::cout << "| [++] C++ LINUX  |  Applications  Places  System  |                                     11:50 AM  [#] [!] |" << std::endl;
+    std::cout << "|========================================================================================================|" << std::endl;
+    
+    
+    std::cout << "|  ____________________________________________________________________________________________________  |" << std::endl;
+    std::cout << "| /  [X] Google - Search  | [+]                                                                    [_|X] |" << std::endl;
+    std::cout << "| |----------------------------------------------------------------------------------------------------| |" << std::endl;
+    std::cout << "| |  < >  (R)  https://www.google.com                                                       [=] [?] [ ]| |" << std::endl;
+    std::cout << "| |----------------------------------------------------------------------------------------------------| |" << std::endl;
+    std::cout << "| |                                                                                                    | |" << std::endl;
+    std::cout << "| |                                                                                                    | |" << std::endl;
+    std::cout << "| |                                     G  o  o  g  l  e                                               | |" << std::endl;
+    std::cout << "| |                                                                                                    | |" << std::endl;
+    std::cout << "| |                          __________________________________________                                | |" << std::endl;
+    std::cout << "| |                         [ Search C++ coming soon...            | Q ]                               | |" << std::endl;
+    std::cout << "| |                          ------------------------------------------                                | |" << std::endl;
+    std::cout << "| |                                                                                                    | |" << std::endl;
+    std::cout << "| |                                [ Google Search ]  [ I'm Feeling Lucky ]                            | |" << std::endl;
+    std::cout << "| |                                                                                                    | |" << std::endl;
+    std::cout << "| |                                                                                                    | |" << std::endl;
+    std::cout << "| |____________________________________________________________________________________________________| |" << std::endl;
+    merory - 400;
+    // Espaço da Área de Trabalho
+    std::cout << "|                                                                                                        |" << std::endl;
+    std::cout << "|  [Trash]                                                                                               |" << std::endl;
+    
+    // Taskbar / Dock
+    std::cout << "|========================================================================================================|" << std::endl;
+    std::cout << "|  [Menu]  ||  (*) Chrome  |  (>) Code  |  (#) Terminal  |  (+) Files  ||  [V] [^] [@@]                  |" << std::endl;
+    std::cout << "|________________________________________________________________________________________________________|" << std::endl;
+        }
+        if (ent == "menu"){
+                std::cout << "__________________________________________________________________________________________" << std::endl;
+    std::cout << "| [++] C++ LINUX OS v1.0   |   Apps   Places   System   |    "<<merory<<" KB's   [#] [!] |" << std::endl;
+    std::cout << "|========================================================================================|" << std::endl;
+
+    // Desktop Area
+    std::cout << "|                                                                                        |" << std::endl;
+    std::cout << "|   [  Home  ]                                                           .------------.  |" << std::endl;
+    std::cout << "|     /--\\                                                               | TERMINAL   |  |" << std::endl;
+    std::cout << "|    |    |                                                              | >_         |  |" << std::endl;
+    std::cout << "|     \\__/                                                               |            |  |" << std::endl;
+    std::cout << "|                                                                        |            |  |" << std::endl;
+    std::cout << "|   [ Trash  ]             __________________________                    |            |  |" << std::endl;
+    std::cout << "|     |XX|                /                          \\                   |            |  |" << std::endl;
+    std::cout << "|     |__|               |      WELCOME TO C++ OS     |                  '------------'  |" << std::endl;
+    std::cout << "|                         \\__________________________/                                   |" << std::endl;
+    std::cout << "|                                                                                        |" << std::endl;
+    std::cout << "|   [Compiler]                                                                           |" << std::endl;
+    std::cout << "|     {OO}                                              [ (X) Close ] [ (-) Min ]        |" << std::endl;
+    std::cout << "|                                                                                        |" << std::endl;
+
+    // Bottom Bar - Taskbar / Dock
+    std::cout << "|========================================================================================|" << std::endl;
+    std::cout << "|  [Menu]  ||  ( ) Browser  |  (>) Code  |  (#) Terminal  |  (+) Files  ||  [V] [^] [@@] |" << std::endl;
+    std::cout << "|________________________________________________________________________________________|" << std::endl;
+    merory - 300;
+        }
+        if (ent == "crash"){
+            merory = 0;
+        }
+        if (merory == 0){
+            std::cout << "__________________________________________________________________________________________________________" << std::endl;
+    std::cout << "| [!!] CRITICAL SYSTEM FAILURE |  Kernel Status: PANIC  |                                 ERROR 0x0000C++ |" << std::endl;
+    std::cout << "|========================================================================================================|" << std::endl;
+    
+    // Espaço em branco superior
+    std::cout << "|                                                                                                        |" << std::endl;
+    
+    // Janela de Erro Centralizada
+    std::cout << "|                 ______________________________________________________________________                 |" << std::endl;
+    std::cout << "|                /                                                                      \\                |" << std::endl;
+    std::cout << "|               |   FATAL EXCEPTION: std::bad_alloc                                      |               |" << std::endl;
+    std::cout << "|               |   Location: Memory Address [0xDEADBEEF]                                |               |" << std::endl;
+    std::cout << "|               |   Reason: Segfault while trying to be too awesome.                     |               |" << std::endl;
+    std::cout << "|               |                                                                        |               |" << std::endl;
+    std::cout << "|               |      ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !       |               |" << std::endl;
+    std::cout << "|               |      !                                                         !       |               |" << std::endl;
+    std::cout << "|               |      !          KERNEL PANIC: ATTEMPT TO KILL INIT!            !       |               |" << std::endl;
+    std::cout << "|               |      !                                                         !       |               |" << std::endl;
+    std::cout << "|               |      ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !       |               |" << std::endl;
+    std::cout << "|               |                                                                        |               |" << std::endl;
+    std::cout << "|               |   [ Reboot System ]          [ Core Dump ]          [ Cry Alone ]      |               |" << std::endl;
+    std::cout << "|                \\______________________________________________________________________/                |" << std::endl;
+    
+    // Fundo da área de trabalho "congelada"
+    std::cout << "|                                                                                                        |" << std::endl;
+    std::cout << "|  ( ) Chrome (Not Responding)                                                                           |" << std::endl;
+    std::cout << "|                                                                                                        |" << std::endl;
+    std::cout << "|                                                                                                        |" << std::endl;
+    
+    // Taskbar corrompida
+    std::cout << "|========================================================================================================|" << std::endl;
+    std::cout << "|  [ERRR]  ||  (X) ERR  |  (X) ERR  |  (X) ERR  |  (X) ERR  ||  [!] [!] [!]                              |" << std::endl;
+    std::cout << "|________________________________________________________________________________________________________|" << std::endl;
+    }
+    }
+    
+    return 0;
+}
